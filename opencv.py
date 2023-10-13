5354688008a5e4e40c918ce8c55eb1f2c1280965
Michael Rolink <482680@student.saxion.nl>
2023-10-13 16:02:53 +0200

    les 2 afgerond

--- opencv.py
+++ opencv.py
@@ -1,5 +1,5 @@
 import os
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,8 +9,29 @@ from urllib.request import urlretrieve
 from IPython.display import Image
 
 
-# Display 18x18 pixel image.
-Image(filename="checkerboard_18x18.png")
+def main():
+    NZ_img_bgr = cv.imread("New_Zealand_Boat.jpg", 1)
+    NZ_img_rgb = cv.cvtColor(NZ_img_bgr, cv.COLOR_BGR2RGB)
 
-# Display 84x84 pixel image.
-Image(filename="checkerboard_84x84.jpg")
+    img_cropped = NZ_img_rgb[200:400, 300:600]
+    plt.imshow(img_cropped)
+    plt.show()
+
+    img_resized = cv.resize(img_cropped, None, fx=2, fy=2)
+    plt.imshow(img_resized)
+    plt.show()
+
+    height = 50
+    width = 100
+    wxh = (width, height)
+
+    img_resized = cv.resize(img_cropped, wxh)
+    plt.imshow(img_resized)
+    plt.show()
+
+    img_flip_hor = cv.flip(img_cropped, 1)
+    plt.imshow(img_flip_hor)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
