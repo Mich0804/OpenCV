06a2430300ec1ae325a407796c03e01c12a87733
Michael Rolink <482680@student.saxion.nl>
2023-10-13 08:46:09 +0200

    begin gemaakt

---
+++ opencv.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from zipfile import ZipFile
+from urllib.request import urlretrieve
+
+from IPython.display import Image
+
+
+# Display 18x18 pixel image.
+Image(filename="checkerboard_18x18.png")
+
+# Display 84x84 pixel image.
+Image(filename="checkerboard_84x84.jpg")
